e106c7d3bbf214a2384710cdf0295222a20c7726
이재호 <ljh2735294@nver.com>
2026-09-21 21:35:53 +0900

    notifications_db_설계

--- backend/src/modules/notifications/entities/notification.entity.ts
+++ backend/src/modules/notifications/entities/notification.entity.ts
@@ -1,1 +1,42 @@
-export class Notification {}
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "../../users/entities/user.entity.js";
+import { NotificationType } from "../enums/NotificationType.enum.js";
+import { ContentsType } from "../enums/ContentsType.enum.js";
+
+@Entity()
+export class Notification {
+    @PrimaryGeneratedColumn('uuid')
+    id!: string;
+
+    @ManyToOne(() => User, (user) => user.notifications)
+    @JoinColumn({ name: 'userId' })
+    user!: User;
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: 'senderId' })
+    sender!: User;
+
+    @CreateDateColumn()
+    createdAt!: Date;
+
+    @DeleteDateColumn()
+    deletedAt!: Date | null;
+
+    @Column({ type: 'enum', enum: NotificationType })
+    type!: NotificationType;
+
+    @Column({type: 'enum', enum: ContentsType})
+    contentsType!: ContentsType;
+
+    @Column()
+    text!: string;
+
+    @Column({default: false})
+    isRead!: boolean;
+
+    @Column()
+    contentsId!: string;
+
+    @Column()
+    linkUrl!: string;
+}
